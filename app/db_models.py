c914cf3cd70b1bcdcb93d1bc4d6b8e5e0d10eb65
Marko Gajic <mgajic@bluecatnetworks.com>
2024-11-13 13:59:13 +0100

    Updated db models

--- app/db_models.py
+++ app/db_models.py
@@ -3,6 +3,7 @@ import uuid
 from sqlalchemy import ForeignKey
 from sqlalchemy.dialects.postgresql import UUID as PG_UUID
 from sqlalchemy.orm import relationship
+from werkzeug.security import generate_password_hash
 
 from app import db
 
@@ -14,6 +15,13 @@ class Member(db.Model):
     password = db.Column(db.String(255), nullable=False)
     age = db.Column(db.Integer, nullable=False)
     phone_number = db.Column(db.String(255))
+
+    def __init__(self, name="", email="", password="", age="", phone_number=""):
+        self.name = name
+        self.email = email
+        self.password_hash = generate_password_hash(password)
+        self.age = age
+        self.phone_number = phone_number
 
 class Author(db.Model):
     __tablename__ = "author"
@@ -42,6 +50,11 @@ class Book(db.Model):
     author = db.relationship("Author", back_populates="books")
     warehouses = db.relationship("WarehouseBook", back_populates="book")
 
+    def __init__(self, title, year_published, author_id):
+        self.title = title
+        self.year_published = year_published
+        self.author_id = author_id
+
 class WarehouseBook(db.Model):
     __tablename__ = "warehouse_book"
     warehouse_id = db.Column(PG_UUID(as_uuid=True), db.ForeignKey("warehouse.id", ondelete="CASCADE"), primary_key=True)
@@ -50,4 +63,9 @@ class WarehouseBook(db.Model):
 
     book = db.relationship("Book", back_populates="warehouses")
     warehouse = db.relationship("Warehouse", back_populates="books")
+
+    def __init__(self, warehouse_id, book_id, quantity):
+        self.warehouse_id = warehouse_id
+        self.book_id = book_id
+        self.quantity = quantity
 
