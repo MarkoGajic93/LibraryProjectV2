c2300f666d6ebab849251244b6364ce26b85255c
Marko Gajic <mgajic@bluecatnetworks.com>
2024-11-14 10:10:31 +0100

    Added logger config and initialization

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 from dotenv import load_dotenv
@@ -9,11 +10,25 @@ class Config:
     POSTGRES_USER = os.environ.get("POSTGRES_USER")
     POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
 
+    LOGGING_LEVEL = logging.INFO
+    LOGGING_FORMAT = '%(asctime)s - %(levelname)s - %(message)s'
+    LOG_TO_FILE = False
+    LOGGING_FILE = os.path.join(os.path.dirname(__file__), 'app.log')
+
     @classmethod
     def get_db_uri(cls, host: str, db_name: str) -> str:
         if not cls.POSTGRES_USER or not cls.POSTGRES_PASSWORD:
             raise ValueError("POSTGRES_USER and POSTGRES_PASSWORD variables must be set.")
         return f"postgresql://{cls.POSTGRES_USER}:{cls.POSTGRES_PASSWORD}@{host}/{db_name}"
+
+    @classmethod
+    def init_logging(cls):
+        handlers = [logging.StreamHandler()]
+        if cls.LOG_TO_FILE:
+            handlers.append(logging.FileHandler(cls.LOGGING_FILE))
+        logging.basicConfig(level=cls.LOGGING_LEVEL,
+                            format=cls.LOGGING_FORMAT,
+                            handlers=handlers)
 
 class DevelopmentConfig(Config):
     DEBUG = True
@@ -25,3 +40,4 @@ class DevelopmentConfig(Config):
     except ValueError as e:
         raise ValueError(f"Failed to configure database: {e}")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    Config.init_logging()
